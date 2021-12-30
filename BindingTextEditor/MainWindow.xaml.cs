a6653239dee729a922ec76575cda8e271c67ed84
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2021-12-30 09:28:43 +0700

    update

--- BindingTextEditor/MainWindow.xaml.cs
+++ BindingTextEditor/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,38 @@ namespace BindingTextEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+        string path = null;
         public MainWindow()
         {
             InitializeComponent();
+            var colors = new List<string> { "Red", "Black", "Yellow" };
+            Select_Color.ItemsSource = colors;
+        }
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            TextB.Text = FileDialogues.OpenFile(out string strPath);
+            path = strPath;
+        }
+
+        private void SaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            FileDialogues.SaveAsFile(TextB.Text, out string strPath);
+            path = strPath;
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (path == null)
+            {
+                FileDialogues.SaveAsFile(TextB.Text, out string strPath);
+                path = strPath;
+                return;
+            }
+            File.WriteAllText(path, TextB.Text);
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
